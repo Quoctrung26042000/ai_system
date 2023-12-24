9762acf0749b7d72411903ca8b478fea67bbf7bc
Quoctrung26042000 <101808329+Quoctrung26042000@users.noreply.github.com>
2023-12-24 19:39:48 +0700

    Add config file

---
+++ config.py
@@ -0,0 +1,14 @@
+##----Config Dir----##
+ANTI_MODEL_DIR = './face_anti/resources/anti_spoof_models'
+DATASET_DIR = 'datasets'
+FACE_ANTI_DIR = './face_anti'
+DEEP_DIR = './deep_face'
+
+#----Config Model----#
+MODEL_NAME = {
+    'facenet':'Facenet512',
+    'arc':'ArcFace',
+    'vgg' :'VGGFace'
+}
+
+MODEL_WEIGHT_DIR = 'weights/model.h5'
